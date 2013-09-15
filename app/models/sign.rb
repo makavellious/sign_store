cbed3982c328320a5c28006fccdfa13d5d8f9ab0
Jeffrey Brock <brockjm@gmail.com>
2013-09-15 16:04:41 -0700

    Add validations to sign

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -2,6 +2,11 @@ class Sign < ActiveRecord::Base
   attr_accessible :dir, :mod, :mod_dir, :name, :quantity
 
   belongs_to :location
+  before_validation :set_defaults
+  validates :name, :mod, :quantity, :price, presence: true
 
-  validates :name, :mod, presence: true
+  def set_defaults
+  	self.price ||= 5
+  	self.quantity ||= 0
+  end
 end
